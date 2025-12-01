131dd45e5e81b1ff9e6ba371b68602beee38ad01
AJ-254 <asiedujuliet20@gmail.com>
2025-12-01 02:47:26 +0000

    linting and autoformatting files

--- db/models/user.py
+++ db/models/user.py
@@ -2,13 +2,12 @@ import enum
 
 from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, Enum, Text
 from sqlalchemy.orm import relationship
-from sqlalchemy.sql.expression import null
 
 from ..db_setup import Base
-from .mixins import Timestamp 
+from .mixins import Timestamp
 
 
-class Role(enum.Enum):
+class Role(enum.IntEnum):
     teacher = 1
     student = 2
 
@@ -24,7 +23,8 @@ class User(Timestamp, Base):
     profile = relationship("Profile", back_populates="owner", uselist=False)
     student_courses = relationship("StudentCourse", back_populates="student")
     student_content_blocks = relationship(
-        "CompletedContentBlock", back_populates="student")
+        "CompletedContentBlock", back_populates="student"
+    )
 
 
 class Profile(Timestamp, Base):
