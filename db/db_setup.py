acd9b2d8b692824e667545d6bfca4b1c58823337
AJ-254 <asiedujuliet20@gmail.com>
2025-12-01 02:06:07 +0000

    adding async db connection; also improving code quality with black and flake8

--- db/db_setup.py
+++ db/db_setup.py
@@ -1,4 +1,5 @@
 from sqlalchemy import create_engine
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
@@ -13,14 +14,20 @@ DB_NAME = os.getenv("POSTGRES_DB")
 DB_HOST = os.getenv("POSTGRES_HOST", "localhost")
 DB_PORT = os.getenv("POSTGRES_PORT", 5432)
 
-DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+# Synchronous and asynchronous database URLs
+SQLALCHEMY_DATABASE_URL = f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+ASYNC_SQLALCHEMY_DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 
-engine = create_engine(
-    DATABASE_URL, connect_args={}, future=True
-)
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={}, future=True)
+async_engine = create_async_engine(ASYNC_SQLALCHEMY_DATABASE_URL, future=True)
+
 SessionLocal = sessionmaker(
     autocommit=False, autoflush=False, bind=engine, future=True
+)
+
+AsyncSessionLocal = sessionmaker(
+    async_engine, class_=AsyncSession, expire_on_commit=False
 )
 
 Base = declarative_base()
@@ -32,3 +39,8 @@ def get_db():
         yield db
     finally:
         db.close()
+
+async def async_get_db():
+    async with AsyncSessionLocal() as db:
+        yield db
+        await db.commit()
